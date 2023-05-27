b440e9b7b530f2c1eab44181e002a4c300c5a52f
Kokeez <jor.reyes@duocuc.cl>
2023-05-27 13:58:08 -0400

    Initial commit

---
+++ MusicProAPIREST/Controllers/TarjetaController.cs
@@ -0,0 +1,75 @@
+﻿using Azure.Core;
+using Microsoft.AspNetCore.Mvc;
+using MusicProAPIREST.Models;
+using MusicProAPIREST.Services;
+
+namespace MusicProAPIREST.Controllers
+{
+    [ApiController]
+    [Route("Tarjeta")]
+    public class TarjetaController
+    {
+        private readonly TarjetaService _ars;
+        public TarjetaController(TarjetaService ars)
+        {
+            _ars = ars;
+        }
+
+        [HttpGet]
+        [Produces("application/json")]
+        public List<Tarjeta> getTarjeta()
+        {
+            return _ars.GetTarjeta();
+        }
+
+
+        [HttpGet("{id}")]
+        [Produces("application/json")]
+        public dynamic getTarjetaPorID(int id)
+        {
+            return _ars.GetTarjetaPorId(id);
+        }
+
+
+        [HttpPost]
+        public dynamic PostTarjeta([FromBody] Tarjeta tarjeta)
+        {
+            return _ars.AgregarTarjeta(tarjeta);
+        }
+
+
+        [HttpPut("{id}")]
+        public dynamic putTarjeta(int id, [FromBody] Tarjeta tarjeta)
+        {
+            return _ars.ModificarTarjeta(id, tarjeta);
+        }
+
+        [HttpDelete("{id}")]
+        public dynamic deleteTarjeta(int id)
+        {
+            return _ars.EliminarTarjeta(id);
+        }
+
+
+
+        [HttpPost("transaccion")]
+        public IActionResult RealizarTransaccion([FromBody] TransaccionRequest request)
+        {
+            try
+            {
+                _ars.RealizarCompra(request.CardNumber, request.carritoId);
+                return new OkObjectResult("La compra se realizó correctamente");
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult(ex.Message);
+            }
+        }
+
+        public class TransaccionRequest
+        {
+            public string CardNumber { get; set; }
+            public int carritoId { get; set; }
+        }
+    }
+}
---
+++ MusicProAPIREST/Models/Tarjeta.cs
@@ -0,0 +1,12 @@
+﻿namespace MusicProAPIREST.Models
+{
+    public class Tarjeta
+    {
+        public int idTarjeta { get; set; }
+        public string cardNumber { get; set; }
+        public string? Nombre { get; set; }
+        public int Saldo { get; set; }
+
+    }
+
+}
